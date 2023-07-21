d43d707c3124c4e0a2362a2ac85794c6adf928bf
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-20 23:16:37 -0300

    configurando jsf

---
+++ src/main/java/br/com/jsf/beans/PessoaBean.java
@@ -0,0 +1,37 @@
+package br.com.jsf.beans;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.jsf.dao.DaoGeneric;
+import br.com.jsf.entidades.Pessoa;
+
+@ViewScoped
+@javax.faces.bean.ManagedBean
+public class PessoaBean {
+	private Pessoa pessoa = new Pessoa();
+	private DaoGeneric<Pessoa>daoGeneric = new DaoGeneric<>();
+	
+	public String salvar () {
+		daoGeneric.salvar(pessoa);
+		return "";
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public DaoGeneric<Pessoa> getDaoGeneric() {
+		return daoGeneric;
+	}
+
+	public void setDaoGeneric(DaoGeneric<Pessoa> daoGeneric) {
+		this.daoGeneric = daoGeneric;
+	}
+	
+	
+}
---
+++ src/main/java/br/com/jsf/dao/DaoGeneric.java
@@ -0,0 +1,18 @@
+package br.com.jsf.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.jsf.jpautil.JPAUtil;
+
+public class DaoGeneric <E>{
+	public void salvar(E entidade) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction transaction =  entityManager.getTransaction();
+		transaction.begin();
+		
+		entityManager.persist(entidade);
+		transaction.commit();
+		entityManager.close();
+	}
+}
